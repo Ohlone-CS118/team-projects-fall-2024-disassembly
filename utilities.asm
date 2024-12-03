4ea0cd710a953e3d92afc54ae83cd50f2a3fc4a2
Ivan9900kar <aryabov1@student.ohlone.edu>
2024-12-02 17:55:45 -0800

    create pop and push for fpu registers

--- utilities.asm
+++ utilities.asm
@@ -9,3 +9,13 @@ subi $sp, $sp, 4
 lw %reg, ($sp)
 addi $sp, $sp, 4
 .end_macro
+
+.macro pushfloat(%reg)
+s.s %reg, -4($sp)
+subi $sp, $sp, 4
+.end_macro
+
+.macro popfloat(%reg)
+l.s %reg, ($sp)
+addi $sp, $sp, 4
+.end_macro
