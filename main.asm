97858a40ead1a5fadac37d8ad9e31b41eacf4c0f
Ivan9900kar <aryabov1@student.ohlone.edu>
2024-12-07 19:27:10 -0800

    Compressing down main

--- main.asm
+++ main.asm
@@ -41,6 +41,7 @@ main:
 .globl level_one
 .globl exit
 
+	# set up keyboard input detection through interrupts
 	move $s2, $zero # set hold counter to 0
 	li $s3, 4	# predefined length to be counted as holding
 
@@ -211,101 +212,66 @@ exit:
 .ktext 0x80000180  
    
 __kernel_entry_point:
-
 	mfc0 $k0, $13		# Get value in cause register.
-
 	andi $k1, $k0, 0x00007c	# Mask all but the exception code (bits 2 - 6) to zero.
-	
-	
-	
 	srl  $k1, $k1, 2	# Shift two bits to the right to get the exception code. 
-	
 	# Now $k0 = value of cause register
 	#     $k1 = exception code 
-	
 	# The exception code is zero for an interrupt and none zero for all exceptions. 
-	
 	beqz $k1, __interrupt 	# branch is interrupt
 	
 __exception:
-
 	# Branch on value of the the exception code in $k1. 
-	
 	beq $k1, 12, __overflow_exception	# branch is overflow code 12
-	
 	beq $k1, 4, __bad_address_exception 	#branch to label __bad_address_exception for exception code 4. 	
-	
 	beq $k1, 13, __trap_exception 	#branch to label __trap_exception for exception code 13. 
 	
-__unhandled_exception: 
-    	
+__unhandled_exception: 	
 	li $v0, 4		# Print the unhandled exception notice
 	la $a0, UNHANDLED_EXCEPTION
 	syscall
- 
  	j __resume_from_exception
 	
 __overflow_exception:
-	
 	li $v0, 4		# Print the overflow exception notice
 	la $a0, OVERFLOW_EXCEPTION
 	syscall
- 
  	j __resume_from_exception
  	
  __bad_address_exception:
-	
 	li $v0, 4		# Print the bad address exception notice
 	la $a0, BAD_ADDRESS_EXCEPTION
 	syscall
- 
  	j __resume_from_exception	
  
 __trap_exception: 
-	
 	li $v0, 4		# Print the bad trap notice
 	la $a0, TRAP_EXCEPTION
 	syscall
- 
  	j __resume_from_exception
 
 __interrupt: 
-
 	# Value of cause register should already be in $k0. 
-    	
     	andi $k1, $k0, 0x00000100	# Mask all but bit 8 (interrupt pending) to zero. 
-    	
-    	# Shift 8 bits to the right to get the inerrupt pending bit as the 
-    	# least significant bit. 
-    	srl  $k1, $k1, 8
-    	
+    	srl  $k1, $k1, 8	# Shift 8 bits to the right to get the inerrupt pending bit as the least significant bit.
     	beq  $k1, 1, __keyboard_interrupt	# Branch on the interrupt pedning bit. 
 
 __unhandled_interrupt: 
-   
   	#  Use the MARS built-in system call 4 (print string) to print error messsage.
-	
 	li $v0, 4		# Print the unhandled Interupt notice
 	la $a0, UNHANDLED_INTERRUPT
 	syscall
- 
  	j __resume
 
 __keyboard_interrupt:     	
 	
 	# Get ASCII value of pressed key from the memory mapped receiver data register. 
-
 	lw $k1, 0xffff0004  # Store content of the memory mapped receiver data register in $k1.
-
 	# Use the MARS built-in system call 11 (print char) to print the character
 	# from receiver data.
-
 	beq $t0, $k1, holdCounter	# if key pressed is the same as previous key pressed
-
 	li $s2, 1 # reset hold counter to 1
-
 	j notHolding	
-
 	holdCounter:
 	addi $s2, $s2, 1		# hold counter++
 
@@ -332,11 +298,8 @@ pressed2:
 bge $s2, $s3, held2 # if key 2 is being held
 #pressed2 content
 
-
 held2:
 #held2 content
-
-###
 
 wInput:
 addi $t1, $t1, 10
@@ -357,9 +320,6 @@ dInput:
 l.s $f30, radianoffset
 sub.s $f0, $f0, $f30
 j __resume
-
-j __resume
-	
 
 __resume_from_exception: 
 	
@@ -384,4 +344,3 @@ __resume:
 	# (PC) to the value saved in the ECP register (register 14 in coporcessor 0).
 	
 	eret # Look at the value of $14 in Coprocessor 0 before single stepping.
-
